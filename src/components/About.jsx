034151fae209c0a0f7b72c5c560ee52287517d16
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-14 19:55:55 +0200

    style: full style of about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
 export default function About({ about }) {
   return (
-    <section className="bg-white text-stone-600">
-      <h2>{about.heading}</h2>
-      <p>{about.text}</p>
-      <img src={about.photo} alt="" />
+    <section className="px-6 py-8 flex flex-col gap-6 bg-white text-stone-600">
+      <h2 className="self-end font-semibold text-stone-400 text-7xl font-heading">{about.heading}</h2>
+      <img src={about.photo} className="max-w-[65%] self-start" alt="" />
+      <p className="pt-8 self-start max-w-[80%]">{about.text}</p>
     </section>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero({ hero }) {
   // {navigation, image, subtitle, description, buttonText}
   return (
-    <section className="pt-2 px-4 flex flex-col gap-0 items-center font-extralight">
+    <section className="pt-2 px-6 flex flex-col gap-0 items-center font-extralight">
       <nav>
         <ul className="flex justify-center gap-4">
           {hero.navigation.map((item) => (
@@ -10,11 +10,11 @@ export default function Hero({ hero }) {
         </ul>
       </nav>
       <img src={hero.image} className="pt-15 max-w-lg w-[70%]" alt="" />
-      <h1 className="text-7xl relative pl-3 -top-11 max-w-[90%] font-semibold font-heading">
+      <h1 className="text-7xl relative self-start pl-2 -top-11 max-w-[90%] font-semibold font-heading">
         {hero.subtitle}
       </h1>
-      <p className="border-l pl-3">{hero.description}</p>
-      <button className="px-12 mt-6 py-2 rounded-md bg-stone-600">
+      <p className="border-l pl-2">{hero.description}</p>
+      <button className="px-15 mt-6 py-3 rounded-sm bg-stone-600">
         {hero.buttonText}
       </button>
     </section>
