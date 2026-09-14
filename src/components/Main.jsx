24025ae5a52e02e3da60cb6f259642df69dcc1a7
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-14 19:08:41 +0200

    feat: pull all data from data.json file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,20 @@ import { hero, about, works, price, contacts } from "../data.json";
 export default function Main() {
   return (
     <main className="h-full flex flex-col text-white bg-[#c5a58e]">
-      <section>{hero.title}</section>
+      <section>
+        <nav>
+          <ul>
+            {hero.navigation.map((item) => (
+              <span>{item}</span>
+            ))}
+          </ul>
+        </nav>
+        <h2>{hero.name}</h2>
+        <h1>{hero.title}</h1>
+        <p>{hero.subtitle}</p>
+        <p>{hero.description}</p>
+        <button>{hero.buttonText}</button>
+      </section>
       <section>{about.heading}</section>
       <section>{works.heading}</section>
       <section>{price.heading}</section>
