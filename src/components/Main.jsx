ae8c431ee68b6d3cf30afb19dd1509d41341cecd
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-14 19:29:15 +0200

    style: basic style for full hero section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,25 @@
 import { hero, about, works, price, contacts } from "../data.json";
+//unused: hero: name, title;;
 
 export default function Main() {
   return (
-    <main className="h-full flex flex-col text-white bg-[#c5a58e]">
-      <section>
+    <main className="h-full flex flex-col gap-10 text-white">
+      <section className="pt-2 px-4 flex flex-col gap-3 items-center">
         <nav>
-          <ul>
+          <ul className="flex justify-center gap-4">
             {hero.navigation.map((item) => (
               <span>{item}</span>
             ))}
           </ul>
         </nav>
-        <h2>{hero.name}</h2>
-        <h1>{hero.title}</h1>
-        <p>{hero.subtitle}</p>
-        <p>{hero.description}</p>
-        <button>{hero.buttonText}</button>
+        <img src={hero.image} className="pt-15 max-w-lg w-[70%]" alt="" />
+        <h1 className="text-6xl relative pl-3 -top-10">{hero.subtitle}</h1>
+        <p className="border-l pl-3">{hero.description}</p>
+        <button className="px-12 mt-2 py-2 rounded-md bg-stone-600">
+          {hero.buttonText}
+        </button>
       </section>
-      <section>{about.heading}</section>
+      <section className="bg-white text-stone-600">{about.heading}</section>
       <section>{works.heading}</section>
       <section>{price.heading}</section>
       <section>{contacts.heading}</section>
