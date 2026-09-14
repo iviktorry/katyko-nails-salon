148a66af613460048d41ef50a406e6a98d247c7c
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-14 19:03:01 +0200

    feat: add basic page sections and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,13 @@
 import { hero, about, works, price, contacts } from "../data.json";
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main className="h-full flex flex-col text-white bg-[#c5a58e]">
+      <section>{hero.title}</section>
+      <section>{about.heading}</section>
+      <section>{works.heading}</section>
+      <section>{price.heading}</section>
+      <section>{contacts.heading}</section>
+    </main>
+  );
 }
