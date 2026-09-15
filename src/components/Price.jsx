05e48f73e901d2ff9d9b4a9a450aa9af8b74d8d7
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-15 13:31:34 +0200

    style: full style of Price component

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,8 +1,8 @@
 export default function Price({ price }) {
   return (
-    <section>
-      <h1>{price.heading}</h1>
-      <ul className="flex flex-col gap-2">
+    <section className="flex flex-col items-center ">
+      <h2 className="self-end font-semibold text-white text-7xl font-heading">{price.heading}</h2>
+      <ul className="flex flex-col gap-3 w-full px-6 py-10 text-stone-600">
         {price.items.map((item) => (
           <li key={item.id} className="flex justify-between">
             <span>{item.service}</span>
@@ -10,7 +10,7 @@ export default function Price({ price }) {
           </li>
         ))}
       </ul>
-      <p>note</p>
+      <p>{price.note}</p>
       <button className="px-15 mt-6 py-3 bg-stone-600">
         {price.buttonText}
       </button>
