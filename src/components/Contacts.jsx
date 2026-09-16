bcfc844d890c4ad1688a6cde9302dac03f2489b1
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-16 11:16:49 +0200

    refactor: fix code to mode semanticly correct, change sources in data

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,15 +1,18 @@
 export default function Contacts({ contacts }) {
   return (
     <section className="px-8 py-8 text-stone-400 bg-white flex flex-col gap-8">
-      <h2 className="self-start font-semibold text-stone-400 text-7xl font-heading">{contacts.heading}</h2>
-      <div className="flex flex-col gap-6">
+      <h2 className="self-start font-semibold text-stone-400 text-7xl font-heading">
+        {contacts.heading}
+      </h2>
+      <ul className="flex flex-col gap-6">
         {contacts.items.map((item) => (
-          <button key={item.id} className="w-full ring-2 py-3 text-left px-3">
-            {item.source}
-            <link rel="stylesheet" href={item.link} className="w-full"/>
-          </button>
+          <li key={item.id} className="w-full ring-2 py-3 text-left px-3">
+            <a href={item.link} className="w-full h-full">
+              {item.source}
+            </a>
+          </li>
         ))}
-      </div>
+      </ul>
     </section>
   );
 }
