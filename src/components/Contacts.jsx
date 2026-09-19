26f41ce6862dd52ab446a14df6424744710b015f
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-19 14:20:32 +0200

    feat: connect form submit to web3forms, receive submitted messages on my email

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,10 +1,40 @@
 import { useTranslation } from "react-i18next";
 import Button from "./Button";
+import { useState } from "react";
 
 export default function Contacts() {
   const { t } = useTranslation();
   const items = t("contacts.items", { returnObjects: true });
-  const style = "ring hover:ring-2 transition-all duration-300 ease-in-out";
+  const style =
+    "ring hover:ring-2 ring-stone-400 text-stone-600 pl-2 transition-all duration-300 ease-in-out";
+
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "437bb0b4-d002-44cf-99e5-a8a148df99a7");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      setResult(data.success ? "Success" : "Error");
+
+      if (data.success) {
+        setResult("Success");
+        event.target.reset();
+      } else {
+        setResult("Error");
+      }
+    } catch (error) {
+      setResult("Error");
+      console.error(error);
+    }
+  };
   return (
     <section
       id="contacts"
@@ -36,14 +66,18 @@ export default function Contacts() {
         </div>
       </div>
 
-      <form className="flex-1 min-w-1 w-full px-6 lg:pl-0 xl:pr-12 self-end">
+      <form
+        onSubmit={onSubmit}
+        className="flex-1 min-w-1 w-full px-6 lg:pl-0 xl:pr-12 self-end"
+      >
         <p>{t("contacts.form.heading")}</p>
         <div className="flex flex-col md:flex-row md:gap-4">
           <label className="flex flex-col flex-1 pb-2 min-w-1 text-sm ">
             {t("contacts.form.nameLabel")}
             <input
               type="text"
-              className={`pl-2 placeholder:text-sm h-7 mt-1 focus:outline-0 focus:ring-stone-600 ${style}`}
+              name="name"
+              className={`placeholder:text-sm h-7 mt-1 focus:outline-0 focus:ring-stone-600 ${style}`}
               placeholder={t("contacts.form.namePlaceholder")}
             />
           </label>
@@ -51,7 +85,8 @@ export default function Contacts() {
             {t("contacts.form.emailLabel")}
             <input
               type="email"
-              className={`pl-2 placeholder:text-sm h-7 mt-1 focus:outline-0 focus:ring-stone-600 ${style}`}
+              name="email"
+              className={`placeholder:text-sm h-7 mt-1 focus:outline-0 focus:ring-stone-600 ${style}`}
               placeholder={t("contacts.form.emailPlaceholder")}
             />
           </label>
@@ -60,14 +95,19 @@ export default function Contacts() {
           <label className="flex-1 flex flex-col text-sm">
             {t("contacts.form.messageLabel")}
             <textarea
-              type="email"
-              className={`mt-1 h-13 focus:outline-0 focus:ring-stone-600 ${style}`}
+              name="message"
+              className={`resize-none mt-1 h-13 pt-1 focus:outline-0 focus:ring-stone-600 ${style}`}
             />
           </label>
           <Button
             text={t("contacts.form.buttonText")}
             style="bg-stone-400 ring ring-stone-400 hover:bg-stone-300 h-13 text-base lg:text-lg"
           />
+          <p className="sr-only" aria-live="polite">
+            {result === "Success"
+              ? t("contacts.form.successMessage")
+              : t("contacts.form.errorMessage")}
+          </p>
         </div>
       </form>
     </section>
